88f760124689d3930f81e92e3553f0160b51f6ad
jingema99 <96966394+jingema99@users.noreply.github.com>
2022-09-05 04:44:39 -0400

    Update glide_util.py

--- glide_finetune/glide_util.py
+++ glide_finetune/glide_util.py
@@ -196,4 +196,100 @@ def sample(
     )[:batch_size]
     glide_model.del_cache()
     return samples
+
+def visual(
+    glide_model,
+    nes_model,
+    glide_options,
+    side_x,
+    side_y,
+    prompt="",
+    batch_size=1,
+    guidance_scale=4,
+    device="cpu",
+    prediction_respacing="100",
+    upsample_enabled=False,
+    image_to_upsample='',
+    upsample_temp=0.997,
+    vocab_dict=None,
+    max_text_len=3,
+):
+    glide_model.del_cache()
+    eval_diffusion = create_gaussian_diffusion(
+        steps=glide_options["diffusion_steps"],
+        noise_schedule=glide_options["noise_schedule"],
+        timestep_respacing=prediction_respacing,
+    )
+    # Create the text tokens to feed to the model.
+    tokens, mask = get_tokens_and_mask(prompt=prompt, max_text_len=max_text_len, vocab_dict=vocab_dict)
+    tokens = tokens.tolist()
+    mask = mask.tolist()
+    print(tokens)
+    print(mask)
+
+    # Create the classifier-free guidance tokens (empty)
+    full_batch_size = batch_size * 2
+    uncond_tokens, uncond_mask = get_uncond_tokens_mask(max_text_len, vocab_dict)
+    uncond_tokens = uncond_tokens.tolist()
+    uncond_mask = uncond_mask.tolist()
+    print(uncond_tokens)
+    print(uncond_mask)
+
+    # Pack the tokens together into model kwargs.
+    model_kwargs = dict(
+        tokens=th.tensor(
+            [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
+        ),
+        mask=th.tensor(
+            [mask] * batch_size + [uncond_mask] * batch_size,
+            dtype=th.bool,
+            device=device,
+        )
+    )
+
+    def cfg_model_fn(x_t, ts, **kwargs):
+        half = x_t[: len(x_t) // 2]
+        combined = th.cat([half, half], dim=0)
+        text_outputs = nes_model(**kwargs)
+        slot = [text_outputs[2]]
+
+        print("visualize slots1")
+        model_out = glide_model(combined, ts, slot)
+        eps, rest = model_out[:, :3], model_out[:, 3:]
+        cond_eps, uncond_eps = th.split(eps, len(eps) // 2, dim=0)
+        beta = eval_diffusion.betas[
+            int(
+                ts.flatten()[0].item()
+                / glide_options["diffusion_steps"]
+                * len(eval_diffusion.betas)
+            )
+        ]
+        half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
+        eps = th.cat([half_eps, half_eps], dim=0)
+        current_prediction_pil = pred_to_pil(
+            (x_t - eps * (beta**0.5))[:batch_size]
+        )
+        current_prediction_pil.save("current_prediction.png")
+        return th.cat([eps, rest], dim=1)
+
+    model_fn = cfg_model_fn # so we use CFG for the base model.
+    if upsample_enabled:
+        assert image_to_upsample != '', "You must specify a path to an image to upsample."
+        low_res_samples = read_image(image_to_upsample, size=(side_x, side_y))
+        model_kwargs['low_res'] = low_res_samples
+        noise = th.randn((batch_size, 3, side_y, side_x), device=device) * upsample_temp
+        model_kwargs['noise'] = noise
+        model_fn = glide_model # just use the base model, no need for CFG.
+
+    samples = eval_diffusion.plms_sample_loop(
+        model_fn,
+        (full_batch_size, 3, side_y, side_x),  # only thing that's changed
+        device=device,
+        clip_denoised=True,
+        progress=True,
+        model_kwargs=model_kwargs,
+        cond_fn=None,
+    )[:batch_size]
+    glide_model.del_cache()
+    return samples
 #glide_util.py
